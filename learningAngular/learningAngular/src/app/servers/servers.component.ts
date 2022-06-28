924d2b69d45f0043b3bf6a8f35a4dd0651a89422
Erick Gruber <erickgruber@outlook.com>
2022-06-28 09:48:55 +0200

    Learning directives

---
+++ learningAngular/learningAngular/src/app/servers/servers.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = 'No Server has been created';
+  serverName = 'Testserver';
+  serverCreated = false;
+  servers = ['Testserver']
+
+  constructor() {
+    setTimeout(()=> {
+      this.allowNewServer = true;
+    },2000)
+  }
+
+  ngOnInit(): void {
+  }
+
+  onCreateServer() {
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
+    this.serverCreationStatus = 'Server CK is created';
+  }
+
+  onUpdateServerName(event:any) {
+    // console.log(event);
+    this.serverName = event.target.value;
+
+  }
+
+}
